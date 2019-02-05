684654d10be62d29169889dc83657873589824bf
Jörg-Christian Böhme <joerg@chaosdorf.de>
2019-02-05 13:27:00 +0100

    Fix src/lib/prov/pkcs11/p11_session.cpp:21:14: runtime error: member call on null pointer of type Botan::PKCS11::LowLevel undefined-behavior src/lib/prov/pkcs11/p11_session.cpp:21

--- session.cpp
+++ session.cpp
@@ -11,12 +11,12 @@ namespace pkcs11 {
 
 std::unique_ptr<Session> Session::create(boost::filesystem::path pkcs11Module, Botan::PKCS11::secure_string password, Botan::PKCS11::SlotId id)
 {
-    Botan::PKCS11::Module module = Botan::PKCS11::Module(pkcs11Module.string());
+    std::unique_ptr<Botan::PKCS11::Module> module = std::make_unique<Botan::PKCS11::Module>(pkcs11Module.string());
     // Sometimes useful if a newly connected token is not detected by the PKCS#11 module
     //module.reload();
 
     // only slots with connected token
-    std::vector<Botan::PKCS11::SlotId> slots = Botan::PKCS11::Slot::get_available_slots( module, true );
+    std::vector<Botan::PKCS11::SlotId> slots = Botan::PKCS11::Slot::get_available_slots(*module, true );
     if(slots.empty())
     {
         throw Botan::PKCS11::PKCS11_Error("No slots found.");
@@ -25,14 +25,14 @@ std::unique_ptr<Session> Session::create(boost::filesystem::path pkcs11Module, B
     Botan::PKCS11::Flags flags =
             Botan::PKCS11::flags( Botan::PKCS11::Flag::SerialSession | Botan::PKCS11::Flag::RwSession );
 
-    Botan::PKCS11::Slot slot(module, id);
+    Botan::PKCS11::Slot slot(*module, id);
 
     std::unique_ptr<Session> s = std::make_unique<Session>(std::move(module), slot, flags, password);
     return s;
 }
 
-Session::Session(Botan::PKCS11::Module module, Botan::PKCS11::Slot slot, Botan::PKCS11::Flags flags, Botan::PKCS11::secure_string password) :
-    module_(std::move(module)), slot_(slot), session_(slot, false)//flags, nullptr, nullptr)
+Session::Session(std::unique_ptr<Botan::PKCS11::Module>&& module, Botan::PKCS11::Slot slot, Botan::PKCS11::Flags flags, Botan::PKCS11::secure_string password) :
+    module_(std::move(module)), slot_(slot), session_(slot_, false)//flags, nullptr, nullptr)
 {
     Botan::PKCS11::SlotId id = slot.slot_id();
     session_.login( Botan::PKCS11::UserType::User, password);
